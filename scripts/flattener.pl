e287b2ea1784449033977a4a0897e26fc7738176
sieferos <sieferos@logtrust.com>
2019-05-12 15:59:09 +0200

    WIP

--- scripts/flattener.pl
+++ scripts/flattener.pl
@@ -19,6 +19,10 @@ my $check = 0;
 my $separator = ',';
 my $quote_char = '"';
 
+use constant {
+  NULL => '-null-',
+};
+
 my $vendor;
 
 sub trim { my @out = @_; for (@out) { s/^\s+//; s/\s+$//; } return wantarray ? @out : $out[0]; } # trim
@@ -27,12 +31,15 @@ sub uppercase { my @out = @_; for (@out) { s/([a-z]+)/uc($1)/eg; } return wantar
 foreach my $a (@ARGV) {
     if ($a =~ /^\-+(DE?B?U?G?)$/i) { $debug = 1; next; }
     if ($a =~ /^\-+(SI?L?E?N?T?)$/i) { $silent = 1; next; }
+    if ($a =~ /^\-+(SEM?I?C?O?L?O?N?)$/i) { $separator = ';'; next; }
 }
 
-my (@INDEX);
+my (@INDEX, $HEADER);
 
+my $i = 0;
 foreach my $l (trim (<STDIN>)) {
-    print sprintf ("( @ ) CSV [ %s ]\n", $l) if (1 and $debug and not $silent);
+    print sprintf ("( @ ) C [ %s ]\n", $l) if (1 and $debug and not $silent);
+    $HEADER = $l if ++$i == 1;
     ### USER_ID,visitorstatus,clientstatus,clientdebt,prepaid_postpaid,navigationspeed,purchasedpackages,loginprofile,ERROR
     ### 15563076,particular postpago,cliente activo,-null-,particular postpago,-null-,one,completo,-null- ]
     ### 15563076,particular postpago,cliente activo,-null-,particular postpago,-null-,one,completo,True ]
@@ -53,15 +60,53 @@ my $csv = Text::CSV->new ({
     sep_char => ','
 }) if @INDEX;
 
+my ($buffer, $CNT);
+
 foreach my $l (@INDEX) {
     if ($csv->parse($l)) {
-        my ($USER_ID, $visitorstatus, $clientstatus, $clientdebt, $prepaid_postpaid, $navigationspeed, $purchasedpackages, $loginprofile, $ERROR) = $csv->fields();
+        my ($USER_ID, $visitorstatus, $clientstatus, $clientdebt, $prepaid_postpaid, $navigationspeed, $purchasedpackages, $loginprofile, $ERROR, $C) = $csv->fields();
 
         next if ($USER_ID eq "USER_ID");
 
-        my $KEY = join ("|", ( $USER_ID, $visitorstatus, $prepaid_postpaid, $purchasedpackages, $loginprofile ));
-        my $VAL = join ("|", ( $clientstatus, $clientdebt, $navigationspeed, $ERROR ));
+        my $KEY = join ($separator, ( $USER_ID, $visitorstatus, $prepaid_postpaid, $purchasedpackages, $loginprofile ));
 
-        print sprintf ("( @ ) L [ %s ] ( %s )\n", $KEY, $VAL) if ($debug and not $silent);
+        $CNT->{$KEY}++;
+
+        my $VAL = join ($separator, ( $clientstatus, $clientdebt, $navigationspeed, $ERROR ));
+
+        print sprintf ("( @ ) L [ %s ] ( %s )\n", $KEY, $VAL) if (1 and $debug and not $silent);
+
+        $buffer->{$KEY}->{'clientstatus'}->{$clientstatus}++ if ($clientstatus ne NULL);
+        $buffer->{$KEY}->{'clientdebt'}->{$clientdebt}++ if ($clientdebt ne NULL);
+        $buffer->{$KEY}->{'navigationspeed'}->{$navigationspeed}++ if ($navigationspeed ne NULL);
+        $buffer->{$KEY}->{'ERROR'}->{$ERROR}++ if ($ERROR ne NULL);
     }
+}
+
+# print Dumper($buffer);
+
+print sprintf ("%s\n", $HEADER) if $HEADER;
+
+my @flattenkeys = qw( clientstatus clientdebt navigationspeed ERROR );
+foreach my $KEY (sort keys %{$buffer}) {
+  my @R;
+    foreach my $W (@flattenkeys) {
+        my @result;
+        if (defined ($buffer->{$KEY}->{$W})) {
+            foreach my $fv (sort keys %{$buffer->{$KEY}->{$W}}) {
+                push (@result, $fv);
+            }
+        }
+        else { push (@result, 'False'); }
+        my $r = join ('|', sort @result);
+        print sprintf ("( @ ) R [ %s: %s ]\n", $W, $r) if (1 and $debug and not $silent);
+        push (@R, $r)
+    }
+    print sprintf ("%s%s%s%s%d\n",
+        $KEY,
+        $separator,
+        join ($separator, @R),
+        $separator,
+        $CNT->{$KEY} || 0
+    );
 }
